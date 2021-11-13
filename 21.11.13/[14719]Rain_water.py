c96b1be79853deef18eb823b76d89a3c0df0e46f
Bae-ChangHyun <matthew624@kakao.com>
2021-11-13 15:56:31 +0000

    today

--- 21.11.13/[14719]Rain_water.py
+++ 21.11.13/[14719]Rain_water.py
@@ -1,3 +1,14 @@
 row,column=map(int,input().split())
-tmp=list(map(int,input().split))
-now laptop is compelte
+tmp=list(map(int,input().split()))
+
+res,index=0,0
+start=tmp[0]
+
+for i in range(len(tmp)-1):
+    if(tmp[i+1]>=start or i+1==len(tmp)-1):
+        start=min(start,tmp[i+1])
+        res=res+start*(i-index)-sum(tmp[index+1:i+1])
+        index=i+1
+        start=tmp[i+1]
+print(res)
+        
