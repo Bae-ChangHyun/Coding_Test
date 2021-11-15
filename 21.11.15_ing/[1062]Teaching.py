962dde4395b43c1d005531802d89387e4e704b90
Bae-ChangHyun <matthew624@kakao.com>
2021-11-16 02:22:20 +0900

    fail

--- 21.11.15_ing/[1062]Teaching.py
+++ 21.11.15_ing/[1062]Teaching.py
@@ -1,19 +1,35 @@
+from itertools import permutations
 n,k=map(int,input().split())
 sum=0
-tmp2=[]
+strA=""
+tmp2=dict()
+ptmp=[]
 
 for i in range(n):
     tmp=input()
     #tmp=[i for i in set(tmp[4:-4])]
+    #일반적인경우는 그냥 아래처럼 하면됨
     tmp=[i for i in set(tmp)]
     tmp.remove('a')
     tmp.remove('n')
     tmp.remove('t')
     tmp.remove('i')
     tmp.remove('c')
-    tmp2+=tmp
-    if(len(tmp)<=k-5):
-        sum+=1
-if(sum>k-5):
-    
-        
+    strA="".join(tmp)
+    ptm=list(permutations(tmp,len(tmp)))
+    print(ptm)
+    for j in ptm:
+        strB="".join(j)
+        if(strB in tmp2):
+            tmp2[strB]+=1
+            break
+        else:
+            if(strB==strA):
+                tmp2[strB]=1       
+tmp2=sorted(tmp2.values(),reverse=True)
+print(tmp2)
+for i in range(k-5):
+    sum+=tmp2[i]
+print(sum)
+
+# 똑같은 방법인데 순열만생각해보자 
