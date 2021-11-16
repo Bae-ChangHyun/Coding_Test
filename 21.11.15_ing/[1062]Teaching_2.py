3ce9ecf48fb799b73d64bf8727564cb45fde97ef
Bae-ChangHyun <matthew624@kakao.com>
2021-11-17 00:43:39 +0900

    failed

---
+++ 21.11.15_ing/[1062]Teaching_2.py
@@ -0,0 +1,46 @@
+from itertools import combinations
+n,k=map(int,input().split())
+word=[]
+letter=[]
+cnt=0
+
+if(k<5):
+    print(0)
+else:
+    for i in range(n):
+        tmp=input()
+        tmp=[i for i in set(tmp)]
+        tmp.remove('a') 
+        tmp.remove('n')
+        tmp.remove('t')
+        tmp.remove('i')
+        tmp.remove('c')
+        for j in tmp:
+            letter.append(j)
+        tmp="".join(tmp)
+        word.append(tmp)       
+    letter=set(letter)
+
+    for i in combinations(letter,k-5):
+        strA="".join(i)
+        for j in word:
+            if(j in strA):
+                cnt+=1
+    print(cnt)
+
+'''
+result=[0]
+for i in range(len(word),0,-1):
+     for j in combinations(word,i):
+        tmp=""
+        for l in j:
+            if(l not in tmp):
+                tmp+=l
+        if(len(tmp)<=k-5):
+            result.append(i)
+        if(len(result)==2):
+            break
+     if(len(result)==2):
+            break 
+print(result)
+'''
