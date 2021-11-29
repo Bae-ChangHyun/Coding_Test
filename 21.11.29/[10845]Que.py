d8fe2653fd4772b0664c780a45956220ba32fc49
Bae-ChangHyun <matthew624@kakao.com>
2021-11-30 02:09:32 +0900

    complete

--- 21.11.29/[10845]Que.py
+++ 21.11.29/[10845]Que.py
@@ -3,24 +3,23 @@ n=int(sys.stdin.readline())
 tmp=[]
 
 for _ in range(n):
-    board=[]
-    board=sys.stdin.readline().split()
-    if(board[0]=="push"):
-        tmp.append(int(board[1]))
-    elif(board[0]=="front"):
+    que=[]
+    que=sys.stdin.readline().split()
+    if(que[0]=="push"):
+        tmp.append(int(que[1]))
+    elif(que[0]=="front"):
         if(tmp==[]):print(-1)
         else:print(tmp[0])
-    elif(board[0]=="back"):
+    elif(que[0]=="back"):
         if(tmp==[]):print(-1)
         else:print(tmp[-1])
-            
-    elif(board[0]=="size"):
+    elif(que[0]=="size"):
         print(len(tmp))
-    elif(board[0]=="pop"):
+    elif(que[0]=="pop"):
         if(tmp==[]):print(-1)
         else:
             print(tmp[0])
             tmp=tmp[1:]
-    elif(board[0]=="empty"):
+    elif(que[0]=="empty"):
         if(tmp):print(0)
         else:print(1)
