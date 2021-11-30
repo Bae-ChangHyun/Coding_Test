ba62ee4eae3576f3a52430b1aa6ae455be1d4e53
Bae-ChangHyun <matthew624@kakao.com>
2021-11-30 23:38:43 +0900

    today

--- 21.11.30/[10799]Iron_bar.py
+++ 21.11.30/[10799]Iron_bar.py
@@ -1,25 +1,50 @@
-cnt,sums=0,0
+"""
+sums=0
 tmp=input()
-while("()" in tmp):
-    tmp=tmp.replace("()","L")
-
-
+tmp=[i for i in tmp]
 for i in range(len(tmp)):
-    if(tmp[i]=='('):
-        for j in range(i+1,len(tmp)):
+    if(tmp[i]==')'):
+        for j in range(i-1,-1,-1):
             if(tmp[j]=='('):
-                break
-            elif(tmp[j]==')'):
-                sums+=tmp[i:j+1].count('L')+1
-                tmp=tmp[:i]+'D'+tmp[i+1:]
-                tmp=tmp[:j]+'D'+tmp[j+1:]
-                break
-    if(tmp[j]==')'):
-        for j in range(i,-1,-1):
-            if(tmp[j]=='('):
-                sums+=tmp[j:i+1].count('L')+1
-                tmp=tmp[:i]+'D'+tmp[i+1:]
-                tmp=tmp[:j]+'D'+tmp[j+1:]
+                if(j==i-1):
+                    tmp[i]='L'
+                    tmp[j]='L' #레이저부분을 저장해두면서 총길이 안바꾸려고 ()를 LL로 바꿔둠
+                    break
+                sums+=(tmp[j:i+1].count('L')//2)+1   #부셔진 조각의 수는 기둥사이에 있는 레이저개수 +1
+                tmp[j]='D' #이미 체크한 기둥의 (부분만 d로바꿔서 체킹해줌 
                 break
 #print(tmp)
 print(sums)
+arr = list(input())
+bar = 0 #현재 짝대기 개수
+cnt = 0 # 정답 
+i = 0
+while 1:
+    if i==len(arr)-1: break
+    if arr[i]=='(':
+        if arr[i+1]==')':
+            cnt+=bar
+            i+=2
+            continue
+        bar+=1
+        cnt+=1
+    else:
+        bar-=1
+    i+=1
+print(cnt)
+"""
+arr = list(input())
+bar,cnt = 0,0 
+for i in range(len(arr)-1):
+    print(i,cnt,bar)
+    if (arr[i]=='('):
+        if (arr[i+1]==')'):
+            cnt+=bar
+            i+=2
+            continue
+        bar+=1
+        cnt+=1
+    elif (arr[i]==')'):
+        bar-=1
+print(i)
+print(cnt)
