ab3ae20e1ef610306eba16791213ccd0eb7e37bb
Bae-ChangHyun <matthew624@kakao.com>
2021-12-01 16:45:52 +0900

    2solve

---
+++ 21.12/21.12.01/[10820]String_analysis.py
@@ -0,0 +1,19 @@
+import sys
+result=[0]*4
+
+while(1):
+    try:
+        tmp=sys.stdin.readline().rstrip('/n')
+        for i in tmp:
+            if('a'<=i<='z'):
+                result[0]+=1
+            elif('A'<=i<='Z'):
+                result[1]+=1
+            elif(i.isdigit()==1):
+                result[2]+=1
+            elif(i==" "):
+                result[3]+=1
+        print(result[0],result[1],result[2],result[3])
+        result=[0]*4
+    except EOFError:
+        break
