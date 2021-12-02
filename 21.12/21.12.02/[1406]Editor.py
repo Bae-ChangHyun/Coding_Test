c412dcd2407110a073b8e8b2017869e0a88e31de
Bae-ChangHyun <matthew624@kakao.com>
2021-12-03 00:26:52 +0900

    fail

--- 21.12/21.12.02/[1406]Editor.py
+++ 21.12/21.12.02/[1406]Editor.py
@@ -1,8 +1,10 @@
+"""
+import sys
 word,n=input(),int(input())
 loc=len(word)
 
 for i in range(n):
-    tmp=input().split()
+    tmp=sys.stdin.readline().rstrip().split()
     if(tmp[0]=='L'):
         if(loc!=0):loc-=1
     elif(tmp[0]=='D'):
@@ -14,5 +16,23 @@ for i in range(n):
     else:
         word=word[:loc]+tmp[1]+word[loc:]
         loc+=1
-    print(word)
 print(word)
+"""
+from sys import stdin
+ 
+stk1 = list(stdin.readline().strip())
+stk2 = []
+n = int(input())
+for line in stdin:
+    if line[0] == 'L':
+        if stk1: stk2.append(stk1.pop())
+        else: continue
+    elif line[0] == 'D':
+        if stk2: stk1.append(stk2.pop())
+        else: continue
+    elif line[0] == 'B':
+        if stk1: stk1.pop()
+        else: continue
+    elif line[0] == 'P':
+        stk1.append(line[2])
+print(''.join(stk1 + list(reversed(stk2))))
