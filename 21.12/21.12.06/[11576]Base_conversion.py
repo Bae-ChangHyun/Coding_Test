18cb4965d67c20cb91ee0d0467b9b1a6830393b8
Bae-ChangHyun <matthew624@kakao.com>
2021-12-07 00:40:11 +0900

    complete1

--- 21.12/21.12.06/[11576]Base_conversion.py
+++ 21.12/21.12.06/[11576]Base_conversion.py
@@ -6,11 +6,13 @@ num=0
 
 word=list(map(int,sys.stdin.readline().split()))
 word.reverse()
-#17진법으로 나타낸 숫자의 자리수의 갯수가 2
-#17진법을 이루고 있는 숫자 2개가 공백을 구분으로 높은 자리부터 차례대로 주어진다.
-16*1+17*2
-
 
 for idx,i in enumerate(word):
-    num+=(idx+1)*i*a
-print(num)
+    num+=i*(a**idx)
+word=[]
+while(num!=0):
+    r=num%b
+    word.append(r)
+    num//=b
+for i in reversed(word):
+    print(i,end="")
