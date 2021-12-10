83caa217386155cb0e7a25dfca6ad64a4ed122f3
Bae-ChangHyun <matthew624@kakao.com>
2021-12-10 15:27:22 +0000

    check

--- 21.12/21.12.09/[1783]Sick_Knight.py
+++ 21.12/21.12.09/[1783]Sick_Knight.py
@@ -1,5 +1,5 @@
 h,w=map(int,input().split())
-if(h==1 or w==1):
+if(h==1):
     answer=1
 elif(h<3):
     answer=(w-1)//2+1
@@ -9,4 +9,5 @@ if(answer>=5):
     if(h>=3):answer=max(4,5+(w-7))
     else:answer=4
 print(answer)
+
     
