ff4571904aa4d75deb53e1230f8255b0e3ff443f
Bae-ChangHyun <matthew624@kakao.com>
2022-01-04 13:06:24 +0000

    1

--- 2022/22.01/22.01.04/[3568]isharp.py
+++ 2022/22.01/22.01.04/[3568]isharp.py
@@ -2,19 +2,22 @@ import sys
 input=sys.stdin.readline
 
 command=list(input().split())
-print(command)
 
 for i in command:
+    flag=0
     if(i==command[0]):
         continue
     i=i[:-1]
     for j in range(len(i)):
         if(i[j].isalpha()==1):
-            tmp=i[j+1:][::-1]
-            print(tmp)
-            i=tmp+i[:j+1]
-            print(i)
+            for k in range(j+1,len(i)):
+                if(i[k].isalpha()==0):
+                    flag=1
+                    break
+            if(flag==1):
+                tmp=i[k:][::-1]+" "+i[j:k]+';'
+            else:
+                tmp=" "+i[j:]+';'
+            tmp=tmp.replace('][','[]')
+            print(command[0]+tmp)
             break
-            
-            
-print(command)
