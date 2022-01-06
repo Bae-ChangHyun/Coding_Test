992f9d18b96a9c91071d4b92f6bf083126814094
Bae-ChangHyun <matthew624@kakao.com>
2022-01-06 17:56:43 +0900

    fucking dp

---
+++ 2022/22.01/22.01.06/[1010]Install_Bridge.py
@@ -0,0 +1,15 @@
+import sys
+input=sys.stdin.readline
+
+t=int(input())
+
+for i in range(t):
+    a,b=1,1
+    w,e=map(int,input().split())
+    for j in range(e,e-w,-1):
+        a=a*j
+    for j in range(w,0,-1):
+        b=b*j
+    print(a//b)
+
+    
