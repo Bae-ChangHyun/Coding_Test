a160f350253c7f8e42bba93c27f464c077c5e2ca
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-08-23 23:30:31 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 124 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,35 @@
+import sys
+input=sys.stdin.readline
+
+stack=[]
+
+while(1):
+    flag=1
+    stack=[]
+    tmp=input().rstrip()
+    if(tmp=='.'):break
+    tmp=[i for i in tmp]
+    for i in tmp:
+        if(i.isalpha() or i=='.' or i==' '):continue
+        else:
+            if(i in ['(','[']):
+                stack.append(i)
+            else:
+                if(stack==[]):
+                    flag=0
+                    break
+                if(i==')'):
+                    if(stack[-1]=='('):
+                        stack.pop()
+                    else:
+                        flag=0
+                        break
+                if(i==']'):
+                    if(stack[-1]=='['):
+                        stack.pop()
+                    else:
+                        flag=0
+                        break
+    if(flag and stack==[]):print("yes")
+    else:print("no")
+        
