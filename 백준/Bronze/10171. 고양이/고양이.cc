f5586be3a862490dcab2e4e793dbbc90fb7a09a2
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-08-23 23:32:04 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h>
+ 
+
+int main(){
+	
+	printf("\\    \/\\\n");
+	printf(" \)  \( \'\)\n");
+	printf("\(  \/  \)\n");
+	printf(" \\\(\_\_\)\|");
+
+	return 0;
+}
