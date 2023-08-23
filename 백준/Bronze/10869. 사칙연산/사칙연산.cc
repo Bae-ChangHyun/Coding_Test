45f0002fce696bab2f563d2a30c6701a502eb507
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-08-23 23:34:04 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h>
+ 
+
+int main(){
+	
+	int A=0,B=0;
+	
+	scanf("%d %d",&A,&B);
+	
+	printf("%d\n",A+B);
+	printf("%d\n",A-B);
+	printf("%d\n",A*B);
+	printf("%d\n",A/B);
+	printf("%d\n",A%B);
+
+	return 0;
+}
