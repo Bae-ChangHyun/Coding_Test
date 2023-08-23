25cd088356848cc2e38ee7d4cf71446b876e6703
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-08-23 23:32:11 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h>
+ 
+
+int main(){
+	
+	printf("\|\\\_\/\|\n");
+	printf("\|\q \p\|   \/\}\n");
+	printf("\( \%d \)\"\"\"\\\n",0);
+	printf("\|\"\^\"\`    \|\n");
+	printf("\|\|\_\/\=\\\\\_\_\|");
+
+	return 0;
+}
