32e5c74b3e9c454f017eae4db37eab172edf7631
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-08-23 23:36:54 +0900

    [Silver V] Title: Base Conversion, Time: 72 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Silver/11576. Base Conversion/Base Conversion.py
@@ -0,0 +1,18 @@
+import sys
+
+a,b=map(int,sys.stdin.readline().split())
+n=int(input())
+num=0
+
+word=list(map(int,sys.stdin.readline().split()))
+word.reverse()
+
+for idx,i in enumerate(word):
+    num+=i*(a**idx)
+word=[]
+while(num!=0):
+    r=num%b
+    word.append(r)
+    num//=b
+for i in reversed(word):
+    print(i,end=" ")
