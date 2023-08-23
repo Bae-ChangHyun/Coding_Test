46f8db3b2f00738bdbb68a66fb53f98bd87fd35b
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-08-23 23:22:19 +0900

    [Silver III] Title: 1로 만들기, Time: 772 ms, Memory: 38664 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,14 @@
+n=int(input())
+
+d=[0]*(n+1)
+d[1]=0
+
+for i in range(2,n+1):
+    tmpa,tmpb=n,n
+    if(i%2==0):
+        tmpa=d[i//2]+1
+    if(i%3==0):
+        tmpb=d[i//3]+1
+    tmpc=d[i-1]+1
+    d[i]=min(tmpa,tmpb,tmpc)
+print(d[n])
