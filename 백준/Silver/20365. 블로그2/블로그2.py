e50ec496c068acb357fecae511d3f69bfcb71542
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-01 16:11:28 +0900

    [Silver III] Title: 블로그2, Time: 140 ms, Memory: 34244 KB -BaekjoonHub

--- 백준/Silver/20365. 블로그2/블로그2.py
+++ 백준/Silver/20365. 블로그2/블로그2.py
@@ -9,12 +9,12 @@ temp=[a[0]]
 for i in range(1,len(a)):
     if(a[i]!=temp[-1] and a[i]!='\n'):
         temp.append(a[i])
-        
+temp='.'.join(temp)
+    
 b=temp.count('B')
 r=temp.count('R')
-         
-for i in range(1,len(a)):
-    if(a[i]!=a[i-1]):cnt+=1
+c=temp.count('.')+1
 
-answer=min(min(b,r)+1,cnt)
+answer=min(min(b,r)+1,c)
+
 print(answer)
