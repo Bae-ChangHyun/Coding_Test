c4335699ac6a91c5ae0eaf917055bc3974d781ce
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-05 17:28:26 +0900

    [Silver II] Title: 유기농 배추, Time: 72 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,38 +1,42 @@
 import sys
-sys.setrecursionlimit(2_501)
 from collections import deque
-input= sys.stdin.readline
 
-t=int(input())
-direct=[[-1,0],[1,0],[0,-1],[0,1]]
+input = sys.stdin.readline
 
-def dfs(idx1,idx2):
-    visit[idx1][idx2]=1
-    for i in direct:
-        n_idx1=idx1+i[0]
-        n_idx2=idx2+i[1]
-        if(n_idx1>=0 and n_idx1<m and n_idx2>=0 and n_idx2<n):
-            if(visit[n_idx1][n_idx2]==0 and graph[n_idx1][n_idx2]==1):
-                dfs(n_idx1,n_idx2)
-                
-    #print("here", idx1,idx2)
-    return 0
+t = int(input())
+direct = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
-for i in range(t):
+def bfs(idx1, idx2):
+    Q = deque()
+    Q.append((idx1, idx2))
+    visit[idx1][idx2] = 1
     
-    m,n,k=map(int,input().split())
-    graph=[[0 for i in range(n)] for j in range(m)]
-    visit=[[0 for i in range(n)] for j in range(m)]
-    cnt=0
+    while Q:
+        idx1, idx2 = Q.popleft()
+        
+        for i in direct:
+            n_idx1 = idx1 + i[0]
+            n_idx2 = idx2 + i[1]
+            
+            if 0 <= n_idx1 < m and 0 <= n_idx2 < n:
+                if visit[n_idx1][n_idx2] == 0 and graph[n_idx1][n_idx2] == 1:
+                    Q.append((n_idx1, n_idx2))
+                    visit[n_idx1][n_idx2] = 1
+
+for _ in range(t):
+    m, n, k = map(int, input().split())
+    graph = [[0] * n for _ in range(m)]
+    visit = [[0] * n for _ in range(m)]
+    cnt = 0
     
-    for j in range(k):
-        a,b=map(int,input().split())
-        graph[a][b]=1  
-          
-    for j in range(m):
-        for k in range(n):
-            if(visit[j][k]==0 and graph[j][k]==1):
-                dfs(j,k)
-                cnt+=1  
-    print(cnt)      
-    #print(graph)
+    for _ in range(k):
+        a, b = map(int, input().split())
+        graph[a][b] = 1
+    
+    for i in range(m):
+        for j in range(n):
+            if visit[i][j] == 0 and graph[i][j] == 1:
+                bfs(i, j)
+                cnt += 1
+    
+    print(cnt)
