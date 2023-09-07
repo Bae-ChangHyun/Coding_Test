1f80d916da4bdf89985b6758df78b2f4c65c73fd
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-07 17:25:05 +0900

    [Gold V] Title: 적록색약, Time: 76 ms, Memory: 34200 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,50 +1,42 @@
 import sys
 from collections import deque
-input= sys.stdin.readline
 
-def bfs(x,y):
-        Q=deque()
-        Q.append((x,y))
-        visit[x][y]=1
-        while Q:
-            x,y=Q.popleft()
-            for i in range(4):
-                n_x,n_y=x+direct[i][0],y+direct[i][1]
-                if(0<=n_x<n and 0<=n_y<n):
-                    if(visit[n_x][n_y]==0 and graph[n_x][n_y]==graph[x][y]):
-                        visit[n_x][n_y]=1
-                        Q.append((n_x,n_y))  
-        return 0
+input = sys.stdin.readline
 
-n=int(input())
-direct=[[1,0],[-1,0],[0,1],[0,-1]]
-graph = [list(input()) for _ in range(n)]
-answer=[]
+def mainS(t):
+    global visit
+    visit = [[0] * n for _ in range(n)]
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if visit[i][j] == 0:
+                if graph[i][j] in ['R','G'] and t == 1:
+                    check=['R','G']
+                else:check = [graph[i][j]]  
+                bfs(i, j, check)
+                cnt += 1
+    return cnt
 
-visit=[[0]*n for _ in range(n)]
-cnt=0
-for i in range(n):
-    for j in range(n):
-        if(visit[i][j]==0):
-            
-            bfs(i,j)
-            cnt+=1
-answer.append(cnt)
+def bfs(x, y, check):
+    Q = deque()
+    Q.append((x, y))
+    visit[x][y] = 1
 
-visit=[[0]*n for _ in range(n)]
-cnt=0
+    while Q:
+        x, y = Q.popleft()
+        for i in range(4):
+            n_x, n_y = x + direct[i][0], y + direct[i][1]
+            if 0 <= n_x < n and 0 <= n_y < n:
+                if visit[n_x][n_y] == 0 and graph[n_x][n_y] in check:
+                    visit[n_x][n_y] = 1
+                    Q.append((n_x, n_y))
 
-for i in range(n):
-    for j in range(n):
-        if graph[i][j]=='G':
-            graph[i][j]='R'
+n = int(input())
+direct = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+graph = [input().rstrip() for _ in range(n)]
+answer = []
 
-for i in range(n):
-    for j in range(n):
-        if(visit[i][j]==0):
-            bfs(i,j)
-            cnt+=1
-answer.append(cnt)
+for i in range(2):
+    answer.append(mainS(i))
 
-print(*answer)
-
+print(answer[0], answer[1])
