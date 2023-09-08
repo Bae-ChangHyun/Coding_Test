33ab8ef5d46a7fa5d73f2ccf70dcbae98ef9b1b6
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-08 20:39:04 +0900

    [Silver I] Title: 숨바꼭질, Time: 188 ms, Memory: 40428 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -9,14 +9,12 @@ def bfs(idx):
         a=Q.popleft()
         direct[2]=a
         if(a==k):
-            return visit[a]-1
+            return visit[a]
         for i in range(3):
-            new_a=a+direct[i]
-            
-            if(new_a<=limit*2):
-                if(new_a>=0 and visit[new_a]==0):
-                    Q.append(new_a)
-                    visit[new_a]=visit[a]+1            
+            new_a=a+direct[i]  
+            if(0<=new_a<=limit*2 and visit[new_a]==0 ):
+                Q.append(new_a)
+                visit[new_a]=visit[a]+1            
 
 input = sys.stdin.readline
 
@@ -24,8 +22,6 @@ n,k=map(int,input().split())
 direct=[-1,1,0]
 
 limit=max(n,k)
-graph=[i for i in range(2*limit+1)]
 visit=[0 for i in range(2*limit+1)]
 
-visit[n]=1
 print(bfs(n))
