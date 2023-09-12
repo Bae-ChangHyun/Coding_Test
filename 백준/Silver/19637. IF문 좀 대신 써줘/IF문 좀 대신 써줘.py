0e39fe182ce9026a27918d0dd7df9575a74215d8
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-12 13:57:03 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 756 ms, Memory: 60708 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
@@ -1,16 +1,19 @@
 import sys
 
-rankn,n = map(int, sys.stdin.readline().split())
-all = [sys.stdin.readline().split() for _ in range(rankn)]
- 
-for i in range(n):
-    tmp=int(sys.stdin.readline())
-    left,right=0,rankn-1
-    while(left<=right):
-        mid=int((left+right)/2)
-        if(tmp>int(all[mid][1])):
-            left=mid+1
-            mid=left
-        else:
-            right=mid-1
-    print(all[mid][0])
+def binary_search(li, n, start, end):
+    if start > end:
+        return li[start][0]
+    
+    mid = (start + end) // 2
+    
+    if int(li[mid][1]) >= n:
+        return binary_search(li, n, start, mid - 1)
+    else:
+        return binary_search(li, n, mid + 1, end)
+
+N, M = map(int, sys.stdin.readline().split())
+li = [sys.stdin.readline().split() for _ in range(N)]
+for _ in range(M):
+    n = int(sys.stdin.readline())
+    result = binary_search(li, n, 0, len(li) - 1)
+    print(result)
