9ea478f2cab09a037088698a1fb8b5cc25ed0f3c
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-13 18:36:24 +0900

    [Silver II] Title: 랜선 자르기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -4,15 +4,15 @@ input=sys.stdin.readline
 k,n=map(int,input().split())
 line=[int(input()) for _ in range(k)]
 
-start,end=0,max(line)+1
-while(start<end):
+start,end=1,max(line)
+while(start<=end):
     
     mid=(start+end)//2
     tmp=sum([i//mid for i in line])
     
     if(tmp<n):
-        end = mid
+        end = mid - 1
     else:
         start = mid + 1
         
-print(end-1)       
+print(end)       
