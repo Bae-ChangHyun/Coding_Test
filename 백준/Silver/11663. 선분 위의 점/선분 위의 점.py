7b207f6b48cb677d373d3eab0629d6ecaa79e29d
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-13 16:10:06 +0900

    [Silver III] Title: 선분 위의 점, Time: 904 ms, Memory: 79416 KB -BaekjoonHub

--- 백준/Silver/11663. 선분 위의 점/선분 위의 점.py
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.py
@@ -6,30 +6,27 @@ point = list(map(int, input().split()))
 point.sort()
 line=[map(int,input().split()) for i in range(m)]
 
-def binary_min(start, end, target):
-    while start <= end:
-        idx = (start + end) // 2
-        mid = point[idx]
-        
-        if target > mid:
-            start = idx + 1
+def lower_bound(start,end,target):
+    while start<=end:
+        mid = (start+end)//2
+        if point[mid] < target:
+            start = mid +1
         else:
-            end = idx - 1
-    return end + 1
+            end = mid-1
+    return start
 
-def binary_max(start, end, target):
-    while start <= end:
-        idx = (start + end) // 2
-        mid = point[idx]
-
-        if target >= mid:
-            start = idx + 1 
+def upper_bound(start,end,target):
+    while start<=end:
+        mid = (start+end)//2
+        if point[mid] <= target:
+            start = mid +1
         else:
-            end = idx - 1
-    return end
+            end = mid-1
+    return start
 
 for least, largest in line:
     start, end  = 0, len(point)-1 # index
-    mini = binary_min(start, end, least)
-    maxi = binary_max(start, end, largest)
-    print(maxi - mini + 1)
+    mini = lower_bound(start, end, least)
+    maxi = upper_bound(start, end, largest)
+    #print(mini,maxi)
+    print(maxi - mini)
