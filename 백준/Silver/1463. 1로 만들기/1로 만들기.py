89d2e1294ec41a405126b68220e56817092559b1
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-14 21:30:38 +0900

    [Silver III] Title: 1로 만들기, Time: 564 ms, Memory: 39200 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,14 +1,14 @@
+import sys 
+input=sys.stdin.readline
+
 n=int(input())
 
-d=[0]*(n+1)
-d[1]=0
+d=[0]*1000001
 
 for i in range(2,n+1):
-    tmpa,tmpb=n,n
-    if(i%2==0):
-        tmpa=d[i//2]+1
-    if(i%3==0):
-        tmpb=d[i//3]+1
-    tmpc=d[i-1]+1
-    d[i]=min(tmpa,tmpb,tmpc)
+    d[i] = d[i-1] + 1 
+    
+    if(i % 2==0): d[i] = min(d[i], d[i//2] + 1)
+    if(i % 3==0): d[i] = min(d[i], d[i//3] + 1)
+    
 print(d[n])
