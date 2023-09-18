1ed84eb99a48d150ded9602d4e205656abcdbfa6
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-18 15:45:44 +0900

    [Silver V] Title: 돌 게임, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,5 +1,14 @@
-n = int(input())
-if n % 2 == 0:
-    print('CY')
-else:
-    print('SK')
+import sys
+
+input = sys.stdin.readline
+
+n=int(input())
+dp=[0]*(n+1)
+
+for i in range(1,n+1):
+    if(i<3):dp[i]=dp[i-1]+1
+    else:dp[i]=min(dp[i-3]+1, dp[i-1]+1)
+
+if(dp[n]%2==1):print("SK")
+else:print("CY")
+#print(dp)
