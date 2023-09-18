1e3cc8e2ca484f3acc9f3b77a1ec7a3202c69cef
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-18 15:27:03 +0900

    [Silver V] Title: 다리 놓기, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,13 +1,14 @@
 import sys
-input=sys.stdin.readline
+import math
+
+input = sys.stdin.readline
 
 t=int(input())
 
 for i in range(t):
-    a,b=1,1
-    w,e=map(int,input().split())
-    for j in range(e,e-w,-1):
-        a=a*j
-    for j in range(w,0,-1):
-        b=b*j
-    print(a//b)
+    n, m= map(int,input().split())
+    answer=math.comb(m, n)
+    print(answer)
+     
+    
+    
