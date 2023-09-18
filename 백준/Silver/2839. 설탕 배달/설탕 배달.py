a1212608f5c81e3f50388f4ca502d986ca2ad4dc
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-18 15:01:15 +0900

    [Silver IV] Title: 설탕 배달, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -6,11 +6,10 @@ n=int(input())
 dp=[-1]*(n+1)
 sugar=[3, 5]
 
-for i in range(n+1):
-    if(i in sugar):dp[i]=1
-    if(i>=3):
-        for j in sugar:
-            if(dp[i-j]!=-1):
-                if(dp[i]==-1):dp[i]=dp[i-j]+1
-                else:dp[i]=min(dp[i-j]+1,dp[i])
+for i in range(3,n+1):
+    if(i==3 or i==5):dp[i]=1
+    for j in sugar:
+        if(dp[i-j]!=-1):
+            if(dp[i]==-1):dp[i]=dp[i-j]+1
+            else:dp[i]=min(dp[i-j]+1,dp[i])
 print(dp[n])
