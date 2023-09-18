e5019f660a939c3eebc6fe0a6e96c00a7fc883b1
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-18 14:41:27 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,8 +1,12 @@
+import sys
+input = sys.stdin.readline
+
 n=int(input())
-pnum=[0,1]
-if(n<2):
-    print(pnum[n])
-else:   
-    for i in range(2,n+1):
-        pnum.append(pnum[i-2]+pnum[i-1])
-    print(pnum[-1])
+
+dp=[0]*(n+2)
+
+dp[0], dp[1]=0, 1
+
+for i in range(2,n+1):
+    dp[i]=dp[i-1]+dp[i-2]
+print(dp[n])
