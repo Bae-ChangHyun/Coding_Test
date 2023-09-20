ccd618002c3e47e8af960db218c71b542c343324
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2023-09-20 19:29:12 +0900

    [Silver II] Title: 연속합, Time: 92 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,15 +1,7 @@
-import sys
-input=sys.stdin.readline
-
-n=int(input())
-num=list(map(int,input().split()))
-dp=[0]*n
-
-for i in range(n):
-    if(num[i]>=0):dp[i]=max(0,dp[i-1])+num[i]
-    else:  
-        if(dp[i-1]+num[i]>=0):
-            dp[i]=dp[i-1]+num[i]
-        else:
-            dp[i]=num[i] 
-print(max(dp))
+n = int(input())
+m = list( map(int, input().split(' ')))
+ 
+for i in range(1, n):
+    m[i] = max(m[i], m[i] + m[i-1])
+    
+print(max(m))
