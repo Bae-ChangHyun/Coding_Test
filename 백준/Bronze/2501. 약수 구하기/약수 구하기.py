908e90e65b715b46e509aee055538beee562ca4b
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2024-01-03 14:23:24 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,9 +1,16 @@
-N,k=input().split()
-N=int(N)
-k=int(k)
+import sys
+import math
+input=sys.stdin.readline
 
-divisor=[i for i in range(1,N+1) if(N%i==0)]
-if(len(divisor)<k):
-    print(0)
-else:
-    print(divisor[k-1])
+N,K= map(int,input().split())
+answer=[]
+
+for i in range(1,int(math.sqrt(N))+1):
+    if(N%i==0):
+        answer.append(i)
+        answer.append(N//i)
+answer=list(set(answer))
+answer.sort()
+if(len(answer)<K):print(0)
+else:(print(answer[K-1]))
+    
