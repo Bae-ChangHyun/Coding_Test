b5179af68712b1be96517b65a63997e99eb75a8b
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2024-01-03 13:39:15 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 972 ms, Memory: 184540 KB -BaekjoonHub

--- 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.py
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.py
@@ -1,9 +1,15 @@
 import sys
 input=sys.stdin.readline
 
+"""
+sort->시간복잡도:O(NlogN)
+min,max-> 시간복잡도:O(N)
+"""
+
 T=int(input())
 for i in range(T):
     N=int(input())
     num=list(map(int,input().split()))
-    num.sort()
-    print(num[0],num[-1])
+    #num.sort()
+    #print(num[0],num[-1])
+    print(min(num),max(num))
