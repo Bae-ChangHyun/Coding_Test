5e54563ab24f3a59d9b540b79fac7d6542ab0491
Bae-ChangHyun <48899047+Bae-ChangHyun@users.noreply.github.com>
2025-06-16 16:28:37 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,26 +1,26 @@
 import sys
-n=int(sys.stdin.readline())
-tmp=[]
 
-for _ in range(n):
-    board=[]
-    board=sys.stdin.readline().split()
-    if(board[0]=="push"):
-        tmp.append(int(board[1]))
-    elif(board[0]=="top"):
-        if(tmp==[]):
-            print(-1)
-        else:print(tmp[-1])
-    elif(board[0]=="size"):
-        print(len(tmp))
-    elif(board[0]=="pop"):
-        if(tmp==[]):
-            print(-1)
+n = int(sys.stdin.readline().strip())
+stack = []
+
+for i in range(n):
+    temp = sys.stdin.readline().rstrip()
+    try: order, num = temp.split(" ")
+    except: order = temp
+    
+    if order == "push":
+        stack.append(num)
+    elif order == "pop":
+        if stack:
+            print(stack.pop())
         else:
-            print(tmp[-1])
-            tmp=tmp[:-1]
-    elif(board[0]=="empty"):
-        if(tmp):
-            print(0)
+            print(-1)
+    elif order == "top":
+        if stack:
+            print(stack[-1])
         else:
-            print(1)
+            print(-1)
+    elif order == "size":
+        print(len(stack))
+    elif order == "empty":
+        print(1 if not stack else 0)
