c62f7d649f848cd54fe6654f6e20ac5337dfea44
Bae-ChangHyun <chbae624@gmail.com>
2026-02-07 14:56:03 +0900

    [Silver II] Title: N과 M (10), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/15664
+
+import sys
+input = sys.stdin.readline
+
+n, m =  map(int,input().split())
+array = sorted([*map(int,input().split())])
+
+def dfs(root):
+    if len(s) == m :
+        print(' '.join(map(str,s)))
+        return 
+    prev = None  # added
+    for i in range(root,len(array)):
+        if visited[i]==1: continue
+        if prev is not None and prev >= array[i]: continue  # added
+        visited[i]=1
+        s.append(array[i])
+        prev = array[i]  # added
+        dfs(i)
+        s.pop()
+        visited[i]=0
+        prev = array[i]
+        
+s = []
+visited = [0]*(len(array))
+dfs(0)
