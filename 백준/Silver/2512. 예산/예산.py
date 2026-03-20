696a57afd65827e6492d31595fbfc23adaa87be2
Bae-ChangHyun <chbae624@gmail.com>
2026-03-20 17:27:49 +0900

    [Silver II] Title: 예산, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,19 +1,26 @@
 import sys
-input= sys.stdin.readline
-from bisect import bisect_left, bisect_right
+input = sys.stdin.readline
 
-N=int(input())
-budget=list(map(int,input().split()))
-target=int(input())
-start,end=0,max(budget)
+N = int(input())
+bud = list(map(int, input().split()))
+M = int(input())
 
-while(start<=end):
-    
-    mid=(start+end)//2
-    tmp=sum([mid if i>=mid else i for i in budget])
-    
-    if(tmp>target):
-        end = mid-1
-    else:
-        start = mid+1   
-print(end)
+# 전체 요청액이 예산 이하이면 최대 요청액을 그대로 출력
+if sum(bud) <= M:
+    print(max(bud))
+    sys.exit(0)
+
+low, high = 0, max(bud)
+answer = 0
+
+while low <= high:
+    mid = (low + high) // 2          # 현재 후보 상한값
+    total = sum(min(b, mid) for b in bud)   # 상한값을 적용했을 때의 합
+
+    if total <= M:                   # 예산을 초과하지 않으면 더 크게 시도
+        answer = mid
+        low = mid + 1
+    else:                            # 초과하면 상한값을 낮춘다
+        high = mid - 1
+
+print(answer)
